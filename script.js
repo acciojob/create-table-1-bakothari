e7d570bccff4d51a4307291d793269b2169bd9e7
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-14 11:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
 function insert_Row() {
-let table = document.getElementById("sampleTable");
-let tableRow = document.createElement("tr");
-let tabelcol1 = document.createElement("td");
-let tabelcol2 = document.createElement("td");
-tableRow.append(tabelcol1,tabelcol2)
-table.prepend(tableRow);
+    let table = document.getElementById("sampleTable");
+    let tableRow = table.insertRow(0); // Insert new row at position 0
+    let tabelcol1 = tableRow.insertCell(0); // Insert new cell at position 0 of the new row
+    let tabelcol2 = tableRow.insertCell(1); // Insert new cell at position 1 of the new row
     tabelcol1.textContent = "New Cell1";
-    tabelcol2.textContent = "New Cell2" 
-
+    tabelcol2.textContent = "New Cell2";
 }
